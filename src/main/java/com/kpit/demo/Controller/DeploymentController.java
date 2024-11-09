a3e10f544a26cafe50555896ec7fd7651b3aae22
@ckinshuk1 <ckinshuk1@gmail.com>
2024-11-09 23:58:44 +0530

    change to check

--- src/main/java/com/kpit/demo/Controller/DeploymentController.java
+++ src/main/java/com/kpit/demo/Controller/DeploymentController.java
@@ -11,6 +11,7 @@ public class DeploymentController {
    
     @GetMapping("/get")
     public String getInformation() {
+        // Text displaying on main page
         return "WELCOME TO JAVA MAVEN PROJECT INTIGRATED WITH DOCKER!!\n MADE BY: KINSHUK CHAUHAN\n EID: 219639";
     }
 }
